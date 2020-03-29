65c36fd698b21cfc2399c5e2320d4e09306e9169
Ashish Kotnala <ashishkotnala1.ak@gmail.com>
2020-03-29 17:22:44 +0530

    initial commit: leafletJS example with GeoJSON

---
+++ src/main/java/com/ashish/leafletjsmaps/LeafletJsMapsApplication.java
@@ -0,0 +1,12 @@
+package com.ashish.leafletjsmaps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeafletJsMapsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeafletJsMapsApplication.class, args);
+    }
+}
---
+++ src/test/java/com/ashish/leafletjsmaps/LeafletJsMapsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashish.leafletjsmaps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeafletJsMapsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
